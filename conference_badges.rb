8049ce0fcb142bb69a9963bffb0a9810a516ad08
Ben Matare <ben.a.matare@gmail.com>
2018-01-20 21:40:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,3 +8,7 @@ end
 def batch_badge_creator name_array
   return name_array.map! {|i| "Hello, my name is #{i}."}
 end
+
+def assign_rooms name
+  return name.map! {|i| "Hello, #{i}! You'll be assigned to room #{i}!"}
+end
