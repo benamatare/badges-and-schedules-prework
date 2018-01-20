03ebbccdece0074bc399152c93e06b8aed173593
Ben Matare <ben.a.matare@gmail.com>
2018-01-20 22:09:12 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,5 +10,6 @@ def batch_badge_creator name_array
 end
 
 def assign_rooms name
-  return name.map!.each_with_index {|value, index| "Hello, #{value}! You'll be assigned to room #{index}!"}
+  name.each {|value| print "Hello, #{value}!"}
+#  return name.map!.each_with_index {|value, index| "Hello, #{value}! You'll be assigned to room #{index}!"}
 end
